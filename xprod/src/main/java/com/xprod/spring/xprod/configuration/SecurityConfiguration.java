b780c22e2b9d12004cb179c23f89019cf60bb83c
Adrien Houen <adhouen@yahoo.com>
2023-01-31 13:31:19 +0100

    20230131

--- xprod/src/main/java/com/xprod/spring/xprod/configuration/SecurityConfiguration.java
+++ xprod/src/main/java/com/xprod/spring/xprod/configuration/SecurityConfiguration.java
@@ -1,22 +1,79 @@
 package com.xprod.spring.xprod.configuration;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.xprod.spring.xprod.constant.SecurityConstant;
+import com.xprod.spring.xprod.constant.filter.JwtAccessDeniedHandler;
+import com.xprod.spring.xprod.constant.filter.JwtAuthenticationEntryPoint;
+import com.xprod.spring.xprod.constant.filter.JwtAuthorizationFilter;
 
 
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfiguration {
+	private JwtAuthorizationFilter jwtAuthorizationFilter;
+	private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private UserDetailsService userDetailsService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
+	
+	@Autowired
+	public SecurityConfiguration(JwtAuthorizationFilter jwtAuthorizationFilter,
+			JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+			UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		super();
+		this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+		this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+		this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+
+
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{ 
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+		
+		
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().authorizeRequests().requestMatchers(SecurityConstant.PUBLIC_URLS).permitAll()
+		.anyRequest().authenticated()
+		.and().exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+		.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+		.and()
+		.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+	}
+	
+//	@Override
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+	
+	
 
     
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       super.configure(auth);
-    }
-    
-    
-    protected void configure(HttpSecurity http) throws Exception {
-       super.configure(http);
-    }
+   
 
 }
